9fe0df888ea67b835d457efcbc02dd0febd398eb
KunalAnand2907 <46574881+KunalAnand2907@users.noreply.github.com>
2019-01-27 22:27:37 +0530

    Add files via upload

---
+++ project.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+class project{
+	
+project()
+{
+}
+
+static void Display()
+{
+project e=new project();	
+}
+
+public static void main(String args[])
+{
+int n;	
+Scanner scan=new Scanner(System.in);	
+System.out.println("Enter project name:");
+scan.nextLine();
+System.out.println("Enter the start date:");
+scan.nextInt();
+scan.nextLine();
+System.out.println("Enter the end date:");
+scan.nextInt();
+scan.nextLine();
+System.out.println("Enter the role:");
+scan.nextLine();
+System.out.println("Enter the value of n:");
+n=scan.nextInt();
+System.out.println("\n");
+System.out.println("Enter the responsibilities::");
+int arr[]=new int[n];
+for(int i=0;i<n;i++)
+{
+	scan.next();
+}	
+project.Display();	
+}
+}
